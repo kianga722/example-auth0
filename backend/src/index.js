6adb104ca6bef285ec91c023a570f6354951cae2
kianga722 <kianga722@gmail.com>
2019-03-19 22:05:42 -0400

    Edit server js file path directory

--- backend/src/index.js
+++ backend/src/index.js
@@ -79,11 +79,11 @@ app.post('/api/answer/:id', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
 
   // Handle React routing, return all requests to React app
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
   });
 }
 
